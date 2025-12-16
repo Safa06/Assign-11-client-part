b17d788cc94505163337e5788f82df732bdc4ad3
Safa06 <sukonna.safa@gmail.com>
2025-12-16 10:47:23 +0600

    commit-5

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,13 +18,18 @@ import axios from "axios";
 import { motion } from "framer-motion";
 import { Link } from "react-router";
 import hero from "../../assets/hero-1.png"
+import shirt1 from "../../assets/shirt-1.jpg"
+import shirt2 from "../../assets/shirt-2.jpg"
+import shirt3 from "../../assets/shirt-3.jpg"
 import { TypeAnimation } from "react-type-animation";
 
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  const [active, setActive] = useState(0);
+  //const [active, setActive] = useState(0);
 
+
+  
   useEffect(() => {
     axios
       .get("http://localhost:3000/products/home")
@@ -145,12 +150,12 @@ const Home = () => {
               "Place Order Easily",
               "Fast & Secure Delivery",
             ].map((step, i) => (
-              <div key={i} className="bg-white p-8 rounded shadow">
-                <div className="text-4xl font-bold text-indigo-600 mb-4">
+              <div key={i} className="bg-green-100 p-8 rounded shadow">
+                <div className="text-4xl font-bold text-red-800 mb-4">
                   {i + 1}
                 </div>
-                <h3 className="text-xl font-semibold mb-2">{step}</h3>
-                <p className="text-gray-600">
+                <h3 className="text-xl font-semibold mb-2 text">{step}</h3>
+                <p className="text-black">
                   Simple, transparent and efficient process.
                 </p>
               </div>
@@ -159,15 +164,20 @@ const Home = () => {
         </div>
       </section>
 
+      
+
       {/*CUSTOMER FEEDBACK CAROUSEL*/}
-      <div className="carousel w-full h-40 bg-green-200 mb-10">
+      <p className="font-bold text-4xl text-green-800 text-center mb-6">
+        Happy Customers !
+      </p>
+      <div className="carousel w-full h-40 text-center bg-red-100 mb-10">
         {/* Slide 1 */}
         <div
           id="slide1"
           className="carousel-item relative w-full flex flex-col items-center"
         >
-          <p className="mt-10 mb-2 text-center text-lg text-green-800 font-semibold">
-            {feedbacks?.[0]?.text}
+          <p className="mt-10 mb-2 text-center text-lg text-red-800 font-semibold">
+            " {feedbacks?.[0]?.text} "
           </p>
           <p className="italic "> - {feedbacks?.[0]?.name}</p>
 
@@ -186,8 +196,8 @@ const Home = () => {
           id="slide2"
           className="carousel-item relative w-full flex flex-col items-center"
         >
-          <p className="mt-10 mb-2 text-center text-lg text-green-800 font-semibold">
-            {feedbacks?.[1]?.text}
+          <p className="mt-10 mb-2 text-center text-lg text-red-800 font-semibold">
+            " {feedbacks?.[1]?.text} "
           </p>
           <p className="italic "> - {feedbacks?.[1]?.name}</p>
 
@@ -206,8 +216,8 @@ const Home = () => {
           id="slide3"
           className="carousel-item relative w-full flex flex-col items-center"
         >
-          <p className="mt-10 mb-2 text-center text-lg text-green-800 font-semibold">
-            {feedbacks?.[2]?.text}
+          <p className="mt-10 mb-2 text-center text-lg text-red-800 font-semibold">
+            " {feedbacks?.[2]?.text} "
           </p>
           <p className="italic "> - {feedbacks?.[2]?.name}</p>
 
@@ -222,23 +232,77 @@ const Home = () => {
         </div>
       </div>
 
+      
+
       {/*EXTRA SECTION 1*/}
-      <section className="bg-red-400 text-white py-20 text-center">
+      <section className="text-green-800 py-10 text-center">
         <h2 className="text-3xl font-bold mb-4">Trusted by Thousands</h2>
-        <p className="text-indigo-100">
-          Quality apparel backed by customer satisfaction.
-        </p>
+        <h1 className="font-semibold mt-6 text-red-300 italic">
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed once, initially
+              "Quality apparel backed ...",
+              1000,
+              "Quality apparel backed ... with customer satisfaction",
+              1000,
+            ]}
+            speed={50}
+            style={{ fontSize: "1.3em" }}
+            repeat={Infinity}
+          />
+        </h1>
+        <div className="max-w-7xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-6 mt-10">
+          <div className="bg-green-100 py-8 px-8 rounded-xl ">
+            <img
+              src={shirt1}
+              className=" w-30 h-30 rounded-4xl mx-auto"
+              alt=""
+            />
+            <h1 className="text-red-800 font-bold text-xl mt-3">T-Shirt</h1>
+            <p className="text-black mt-3">
+              Comfortable cotton wear for everyday use. Easy to style with jeans
+              or shorts. Button-up top suitable for formal and casual occasions.
+              Offers a clean and polished look.
+            </p>
+          </div>
+          <div className="bg-green-100 py-8 px-8 rounded-xl ">
+            <img
+              src={shirt2}
+              className=" w-30 h-30 rounded-4xl mx-auto"
+              alt=""
+            />
+            <h1 className="text-red-800 font-bold text-xl mt-3">Jacket</h1>
+            <p className="text-black mt-3">
+              Stylish outerwear that adds warmth and protection. Suitable for
+              casual or semi-formal wear. Warm and cozy sweatshirt with a hood.
+              Perfect for casual outings and cold weather.
+            </p>
+          </div>
+          <div className="bg-green-100 py-8 px-8 rounded-xl ">
+            <img
+              src={shirt3}
+              className="mx-auto w-30 h-30 rounded-4xl"
+              alt=""
+            />
+            <h1 className="text-red-800 font-bold text-xl mt-3">Dress</h1>
+            <p className="text-black mt-3">
+              One-piece outfit offering comfort and style. Available in casual
+              and formal designs. Structured jacket for a professional look.
+              Ideal for office and formal events.
+            </p>
+          </div>
+        </div>
       </section>
 
       {/* EXTRA SECTION 2*/}
-      <section className="max-w-7xl mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold mb-6">
+      <section className="max-w-7xl mx-auto px-6 mt-10 text-center">
+        <h2 className="text-3xl font-bold mb-6 text-green-800">
           Ready to Upgrade Your Style?
         </h2>
         <Link
           to="/products"
-          className="inline-block px-6 py-3 bg-indigo-600 text-white rounded
-                     border border-transparent hover:border-indigo-600 hover:bg-white hover:text-indigo-600
+          className="inline-block px-6 py-3 bg-green-800 text-white rounded-xl font-bold
+                     border-2 border-transparent hover:border-red-800 hover:bg-white hover:text-red-800
                      transition-all duration-300"
         >
           Get Started
